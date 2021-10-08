24d2f97b54f6e7a777fb6ffad8e5f420ff8a6c3d
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-10-08 18:27:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+const dodger = document.querySelector ('#dodger')
+console.dir(dodger);
+
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight()
+    }  
+})
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }  
+})
+
+
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+function moveDodgerLeft () {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+
+
+
